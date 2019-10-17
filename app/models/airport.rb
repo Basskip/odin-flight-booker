5b080e656083dd3a2e46623e3b685a155db5c4e2
Cameron Scott <camscottgreen@hotmail.com>
2019-10-17 11:38:00 +1000

    Add Airport and Flight models

---
+++ app/models/airport.rb
@@ -0,0 +1,4 @@
+class Airport < ApplicationRecord
+    has_many :incoming_flights, class_name: "Flight", foreign_key: "destination_id" 
+    has_many :outgoing_flights, class_name: "Flight", foreign_key: "origin_id"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+25.times do 
+    Airport.create(code: ('A'..'Z').to_a.sample(3).join)
+end
+
+50.times do
+    airports = Airport.all.sample(2)
+    start = rand(6.months).seconds.from_now
+    duration = rand(1.day)
+    Flight.create(origin: airports[0], destination: airports[1], start: start, duration: duration)
+end
